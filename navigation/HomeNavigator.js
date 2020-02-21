38cac08c7e26ed6aa484f83d7cec136c5e4fd615
Alex Smetukh <alex.smetukh@gmail.com>
2020-02-21 16:31:04 +0200

    Add home screens

---
+++ navigation/HomeNavigator.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/home/HomeScreen';
+import ProfileScreen from '../screens/home/ProfileScreen';
+import NotificationsScreen from '../screens/home/NotificationsScreen';
+import SettingsScreen from '../screens/home/SettingsScreen';
+
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    return (
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    );
+  }
+
+  export default MyStack;
---
+++ screens/home/NotificationsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+
+export default function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
