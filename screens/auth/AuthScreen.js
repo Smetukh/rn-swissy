12471740fda0fa4000752d17e0d1712fa67989c2
Alex Smetukh <alex.smetukh@gmail.com>
2020-02-23 13:35:59 +0200

    add state to save input fields upon submit

--- screens/auth/AuthScreen.js
+++ screens/auth/AuthScreen.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useReducer, useCallback } from 'react';
 import { ScrollView, StyleSheet, Text, View, KeyboardAvoidingView, Button } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 
 import Input from '../../components/UI/Input';
 
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
+
 export default AuthScreen = ({ navigation }) => {
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: '',
+      password: ''
+    },
+    inputValidities: {
+      email: false,
+      password: false
+    },
+    formIsValid: false
+  });
+
+  const signupHandler = () => {
+    console.log('formState.inputValues.email, = ', formState.inputValues.email,)
+    console.log('formState.inputValues.password, = ', formState.inputValues.password,)
+    // dispatch(
+    //   authActions.signup(
+    //     formState.inputValues.email,
+    //     formState.inputValues.password
+    //   )
+    // );
+  };
+  
+  const inputChangeHandler = useCallback(
+    (inputIdentifier, inputValue, inputValidity) => {
+      console.log('inputValue = ', inputValue);
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier
+      });
+    },
+    [dispatchFormState]
+  );
+
   return (
     <KeyboardAvoidingView 
       behavior='padding'
@@ -23,7 +83,7 @@ export default AuthScreen = ({ navigation }) => {
             autoCapitalize="none"
             errorText="Please enter a valid email address"
             // onValueChange={() => {}}
-            onInputChange={() => {}}
+            onInputChange={inputChangeHandler}
             initialValue=""
             placeholder="Enter your name"
             style={styles.inputContainer}
@@ -37,13 +97,13 @@ export default AuthScreen = ({ navigation }) => {
             minLength={8}
             autoCapitalize="none"
             errorText="Please enter a valid password"
-            onInputChange={() => {}}
+            onInputChange={inputChangeHandler}
             initialValue=""
             placeholder="Enter your password"
             style={styles.inputContainer}
           />
-          <Button title="Login" color="red" onPress={() => {}} />
-          <Button title="Sign up" color="green" onPress={() => {}} />
+          <Button title="Sign up" color="red" onPress={signupHandler} />
+          <Button title="Sign up" color="green" onPress={signupHandler} />
           <Button
             title="Go to Home"
             onPress={() => navigation.navigate('Home')}
